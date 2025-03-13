3a6598e9a79ff4e37c612cf0cd9b7b907808b224
mkt0416 <mktyk7019@gmail.com>
2025-03-13 16:14:24 +0900

    feat fontend

---
+++ app/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                ©{new Date().getFullYear()} Next Market
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
